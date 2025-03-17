028208f8a11387475708ffa8e5f4430bf37efff3
nsannier <Naofumi76@users.noreply.github.com>
2025-03-17 13:08:49 +0100

    Added function to get the modules of user, and xp from module

--- src/js/profile.js
+++ src/js/profile.js
@@ -3,7 +3,10 @@ import * as audit from './audit.js'
 
 export async function loadProfilePage(token, userInfo) {
 	const audits = await fetchUserAudit(token, userInfo.login, true)
-	console.log(userInfo.login)
+	const xp = await fetchUserXP(token)
+	const modules = await fetchUserModule(token)
+	console.log("XP:", xp)
+	console.log("Modules:", modules)
 	console.log(audits)
 	document.body.innerHTML = `
         <div class="profile-container">
@@ -90,9 +93,71 @@ export async function fetchUserAudit(token, userName, onlyLast = false) {
 	}
 	const data = await response.json()
 
-	// Entire Data Structure
-	console.log(data)
 
 	return data.data.audit
 }
 
+export async function fetchUserXP(token, id=0) {
+	const response = await fetch('https://zone01normandie.org/api/graphql-engine/v1/graphql', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${token}`
+		},
+		body: JSON.stringify({
+			query: `
+				query {
+					transaction_aggregate(where: {
+						type: { _eq: "xp" },
+						eventId: {_eq: ${id}}
+					}) {
+						aggregate {
+							sum {
+								amount
+							}
+						}
+					}
+				}
+            `
+		})
+	});
+	if (!response.ok) {
+		throw new Error('Failed to fetch user info')
+	}
+	const data = await response.json()
+
+	return data.data.transaction_aggregate.aggregate.sum
+}
+
+export async function fetchUserModule(token) {
+	const response = await fetch('https://zone01normandie.org/api/graphql-engine/v1/graphql', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${token}`
+		},
+		body: JSON.stringify({
+			query: `
+				query {
+					user {
+						events(where:{ event: { object: { type : { _in: ["piscine", "module"] }}}}) {
+						event {
+						id
+						object {
+							name
+							type
+								}
+							}
+						}
+					}
+				}
+            `
+		})
+	});
+	if (!response.ok) {
+		throw new Error('Failed to fetch user info')
+	}
+	const data = await response.json()
+
+	return data.data.user[0].events
+}
